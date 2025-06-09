dbfbdb9f2659cddfcb178d56e06e6822cbeca282
Vini <vini@teste>
2025-06-09 19:47:48 -0300

    configurando ajustes

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,64 @@
-// gulpfile.js
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const clean = require("gulp-clean");
+const htmlmin = require("gulp-htmlmin");
+const cleanCSS = require("gulp-clean-css");
 
 // Caminhos
 const paths = {
-  scss: {
-    src: "./scss/**/*.scss",
-    dest: "./css",
-  },
+  html: "./index.html",
+  scss: "./scss/style.scss",
+  css: "./css",
+  dist: "./dist",
 };
 
-// Tarefa de compilação SASS
+// Compilar SASS
 function compilarSass() {
   return gulp
-    .src(paths.scss.src)
+    .src(paths.scss)
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-    .pipe(gulp.dest(paths.scss.dest));
+    .pipe(gulp.dest(paths.css));
 }
 
-// Watch
-function watchFiles() {
-  gulp.watch(paths.scss.src, compilarSass);
+// Limpar dist/
+function limparDist() {
+  return gulp.src(paths.dist, { allowEmpty: true, read: false }).pipe(clean());
 }
 
-// Tarefa padrão
-exports.default = gulp.series(compilarSass, watchFiles);
+// Copiar HTML para dist/
+function copiarHtml() {
+  return gulp
+    .src(paths.html)
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(paths.dist));
+}
+
+// Copiar CSS para dist/
+function copiarCss() {
+  return gulp
+    .src("./css/style.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest(`${paths.dist}/css`));
+}
+
+// Copiar imagens (opcional)
+function copiarImgs() {
+  return gulp.src("img/**/*").pipe(gulp.dest("dist/img"));
+}
+
+function copiarImgs() {
+  return gulp.src("img/**/*", { allowEmpty: true }) // 👈 adiciona allowEmpty
+    .pipe(gulp.dest("dist/img"));
+}
+
+// Tarefa de build
+exports.build = gulp.series(
+  compilarSass,
+  limparDist,
+  gulp.parallel(copiarHtml, copiarCss, copiarImgs)
+);
+
+// Watch (opcional durante dev)
+exports.default = function () {
+  gulp.watch("./scss/**/*.scss", compilarSass);
+};
